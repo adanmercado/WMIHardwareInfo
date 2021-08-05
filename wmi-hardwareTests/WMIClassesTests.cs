f3f8363bf285b3ec0fed9794e8f5b0f2cd2c3e21
Mohammad Yaser Ammar <46596472+MohammadYAmmar@users.noreply.github.com>
2021-08-05 15:18:54 +0300

    Finish test of WMIClasses 🧪
    The test of the contents file containing two functions has been fully completed and the test is successful.
     There are some improvements that will be required to be developed in the future to be more accurate like compare more component not just one, but that require addental library

---
+++ wmi-hardwareTests/WMIClassesTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using wmi_hardware;
+
+//Fork by: Mohammad Yaser Ammar
+//https://github.com/MohammadYAmmar/
+//Feture 1:
+//Add Unit test project
+
+
+namespace wmi_hardwareTests
+{
+    [TestClass]
+    public class WMIClassesTests
+    {
+        //Given_When_Then
+
+        //Success
+        [TestMethod]
+        public void GetAllWMIClasses_Return_success()
+        {
+            ///AAA
+
+            //Arrange
+
+            WMIClasses.GetAllWMIClasses();
+
+
+            //Act
+            //Assert
+
+            //It is correct if it does not return an exception error
+
+        }
+        [TestMethod]
+        public void GetPropertiesForWMIClass_When_ClassWin32_Processor_Return_name_CPU()
+        {
+
+            List<string> actualList = WMIClasses.GetPropertiesForWMIClass("Win32_Processor");
+
+            //Error
+            //System.IO.DirectoryNotFoundException: Could not find a part of the path 'C:\Users\Mohammad Yaser Ammar\Documents\GitHub\WMIHardwareInfo\wmi-hardwareTests\bin\wmiclasses\WMIClassProperties.xml'.
+            //Then but file here #todo !!
+
+
+            string expected = "Name";
+            List<string> expectedList = new List<string> {"Name", "NumberOfCores", "NumberOfLogicalProcessors"};
+
+
+            //Assert
+            CollectionAssert.Contains(actualList, expected);
+
+            //#todo with other library
+            //CollectionAssert.Contains(actualList, expectedList);
+
+
+
+        }
+
+    }
+}
